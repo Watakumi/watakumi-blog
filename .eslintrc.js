57f34063d3dcb8570084e643a53102942cd3ec59
石渡拓海 <t-ishiwata@sight-visit.com>
2021-03-24 05:15:06 +0900

    [Add] Add @typescript-eslint/object-curly-spacing to eslint eslintのobject-curly-spacingと@typescript-eslint/object-curly-spacingが競合していたためうまくeslint --fixが作用していなかった

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,5 +25,6 @@ module.exports = {
     '@typescript-eslint/no-unsafe-return': 'off',
     '@typescript-eslint/comma-dangle': 'off',
     '@typescript-eslint/no-confusing-void-expression': 'off',
+    '@typescript-eslint/object-curly-spacing': ['error', 'always'],
   },
 };
